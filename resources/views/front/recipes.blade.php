4c57826cf0b4458cce42b83d52878759527a4e3f
abanoub899 <abanobgayed@outlook.com>
2021-04-26 23:44:06 +0200

    ababnoub

--- resources/views/front/recipes.blade.php
+++ resources/views/front/recipes.blade.php
@@ -47,13 +47,17 @@
 	<div class="row div_row">
 		@foreach($rc_data as $item)
 		<div class="col-sm-4">
-			<div class="card">
-				<a> 
-					<img src="{{asset('images')}}/{{$item->image}}" alt="Card image" 
+			<div class="card clearfix">
+				<a>
+					<img src="{{asset('images')}}/{{$item->image}}" alt="Card image"
 				    class=" img-responsive rounded"  height="100px">
 				</a>
-				<h5 class="rec_name">  This Recipe Named  "{{$item->name}}"</h5> 
-				<p class="text-center btn"><a href="{{route ('rcps.show',$item)}}"class="button">View More</a></p>
+				<h5 class="rec_name">  This Recipe Named  "{{$item->name}}"</h5>
+				<p class="text-center btn  " ><a href="{{route ('rcps.show',$item)}}"class="button">View More</a></p>
+                {{-- <i class="bi bi-cart"></i> <p class="text-center btn"><a href="{{route ('rcps.show',$item)}}"class="button">Add To Cart</a></p> --}}
+                <p class="bi bi-cart pull-right" ><a href="{{route ('rcps.show',$item)}}"class="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </svg></a></p>
 			</div>
 		</div>
 		@endforeach
@@ -65,10 +69,10 @@
 
 @section('scripts')
 <script>
-	$(document).ready(function() 
+	$(document).ready(function()
 	{
 
-		$('select[name="section"]').on('change', function() 
+		$('select[name="section"]').on('change', function()
 		{
 			var sectionId = $(this).val();
 			// console.log(sectionId);
@@ -79,7 +83,7 @@
 					dataType: "json",
 					success: function(data) {
                         console.log(data);
-					
+
 					},
 				});
 			} else {

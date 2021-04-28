b5c92295f8df7c3d69748b925ce5c8f65ffd3c7f
M.M.alrays <Ma7moudAlrays01200@gmail.com>
2021-04-28 14:19:44 +0200

    M7moud Alrays

--- resources/views/front/shoppingCart.blade.php
+++ resources/views/front/shoppingCart.blade.php
@@ -30,11 +30,12 @@
                   <li class="lis-group-item">
                   <span class="badge text-center ">{{$reciepe['qty']}}</span>
                   <h5 class="rec_name">{{$reciepe['item']['name']}}</h5>
-                  <span class="label label-success">{{$reciepe['item']['price']}}</span>
-                  <img src="{{asset('images')}}/{{$reciepe['item']['image']}}" alt="Card image"class=" img-responsive rounded"  height="100px">
+                  <h5 style="margin-bottom: 10px "><span class="label label-success">{{$reciepe['item']['price']}}</span></h5>
+                  <img src="{{asset('images')}}/{{$reciepe['item']['image']}}" 
+                       alt="Card image"class=""  height="40%" width="100%">
                   <div class="btn-group">
                       <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">Remove<span class="caret"></span></button>
-                      <ul class="dropdown-menu">
+                      <ul class="dropdown-menu" style="margin: 10px" >
                           <li ><a href="#">Remove by 1</a></li>
                           <li><a href="#"> Remove by All</a></li>
                       </ul>

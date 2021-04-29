5c2e170e7fe32d186426f7adf04e5bc6f1f8c830
abanoub899 <abanobgayed@outlook.com>
2021-04-29 03:33:10 +0200

    abanoub

--- resources/views/front/shoppingCart.blade.php
+++ resources/views/front/shoppingCart.blade.php
@@ -32,12 +32,12 @@
                   <span class="badge text-center ">{{$reciepe['qty']}}</span>
                   <h5 class="rec_name">{{$reciepe['item']['name']}}</h5>
                   <h5 style="margin-bottom: 10px "><span class="label label-success">{{$reciepe['item']['price']}}</span></h5>
-                  <img src="{{asset('images')}}/{{$reciepe['item']['image']}}" 
+                  <img src="{{asset('images')}}/{{$reciepe['item']['image']}}"
                        alt="Card image"class=""  height="40%" width="100%">
                   <div class="btn-group">
                       <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">Remove<span class="caret"></span></button>
                       <ul class="dropdown-menu" style="margin: 10px" >
-                          <li ><a href="#">Remove by 1</a></li>
+                          <li ><a href="{{route('rcps.reduceByOne',['id'=>$reciepe['item']['id']])}}">Remove by 1</a></li>
                           <li><a href="#"> Remove by All</a></li>
                       </ul>
                   </div>
@@ -53,6 +53,10 @@
 <div class="row" style="text-align: center ; margin:20px 0px">
     <strong class="alert alert-success">Total Price :  {{$totalPrice}} &dollar;</strong>
     <a href="{{route('rcps.checkout')}}" class="btn btn-success" style="font-size: 25px">CheckOut</a>
+</div>
+<div class="row" style="text-align: center ; margin:20px 0px">
+    <strong class="alert alert-success">Total Price :  {{$totalPrice}} &dollar;</strong>
+    <a href="{{route('rcps.cash')}}" class="btn btn-primary" style="font-size: 25px">Pay cash</a>
 </div>
 
 @else

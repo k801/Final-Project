8e2993eec62dd9b8b4c778fdd19a1f5753800b14
M.M.alrays <Ma7moudAlrays01200@gmail.com>
2021-05-02 17:34:40 +0200

    M7moud Alrays

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -57,7 +57,7 @@
 	</div>
 	<div class="row div_row">
 		@foreach($rc_data as $item)
-		<div class="col-sm-12 col-md-4">
+		<div class="col-xs-12 col-sm-4  col-md-4">
 			<div class="card">
 				
 				<img src="{{asset('images')}}/{{$item->image}}" alt="Card image" 
